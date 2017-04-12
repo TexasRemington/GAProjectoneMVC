277a0cdfe7f71a890b8e4acd13d72e60c96bcabc
melissa pringle <melissa.a.acree@gmail.com>
2017-04-12 11:17:28 -0500

    tues

--- backend/models/animal.js
+++ backend/models/animal.js
@@ -3,10 +3,18 @@ ObjectId = mongoose.Schema.ObjectId;
 
 var animalSchema = new mongoose.Schema({
   name: {type: String, required: true},
-  description: {type: String, required: true},
+  species: {type: String, required: true},
   breed: {type: String, required: true},
+  size: {type: String, required: true},
+  gender: {type: String, required: true},
+  hairType: {type: String, required: true},
+  family: {type: String},
+  energyLevel: {type: String, required: true},
+  trainingNeeds: {type: String, required: true},
+  dependency: {type: String, required: true},
+  hypoallergenic: {type: String, required: true},
+  imageUrl: {type: String, required: true},
   shelter: {type: ObjectId, required: true},
-  image: String,
 });
 
 var Animal = mongoose.model('Animal', animalSchema);
